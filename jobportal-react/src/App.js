a8e2d97c6a7cc5627f31c69a80c317b19aa91bf3
shru <shruthishekar@gmail.com>
2023-10-29 22:55:56 -0600

    header

--- jobportal-react/src/App.js
+++ jobportal-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Header from "./Components/Header"; 
 function App() {
   return (
     <div className="JobPortal">
-      
+     <Header></Header>
     </div>
   );
 }
---
+++ jobportal-react/src/Components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <Container>
+                <Row>
+                    
+                    <Col sm={8} className='title'>Job Portal</Col>
+                    
+                    
+                    <Col sm={4} className='jp'>
+                        <Button variant="info" size="lg" className='jobposting'>Job-Posting</Button>
+                    </Col>
+                    
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Header;
+
