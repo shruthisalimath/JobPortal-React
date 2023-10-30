c64de2b0fd719e101031cccb387a1f24fbf50f13
shru <shruthishekar@gmail.com>
2023-10-30 00:01:01 -0600

    searchbar

--- jobportal-react/src/App.js
+++ jobportal-react/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./Components/Header"; 
+import SearchBar from "./Components/SearchBar"
 function App() {
   return (
     <div className="JobPortal">
      <Header></Header>
+     <div className="searchbar">
+     <SearchBar></SearchBar>
+     </div>
     </div>
   );
 }
---
+++ jobportal-react/src/Components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+const SearchBar = () => {
+    return (
+        <div className="search-bar">
+            <Container >
+                <Form>
+                    <Row>
+                        <Form.Group as={Col} className="Job-Duration">
+                            <Form.Select  defaultValue="Full-time">
+                                <option value="Full-time" >Full-time</option>
+                                <option value="Part-time" >Part-time</option>
+                                <option value="Contract" >Contract</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group as={Col} className="Job-Location">
+                            <Form.Select defaultValue="On-Site">
+                                <option value="On-Site">On-Site</option>
+                                <option value="Remote"  >Remote</option>
+                                <option value="Hybrid" >Hybrid</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group as={Col} className="Job-Search">
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Container>
+        </div>
+    );
+};
+
+export default SearchBar;
